472df7574790696cc1d6a3e9d43847f43068b33d
JulCzar <julio.monteiro@tonolucro.com.br>
2022-05-16 20:42:37 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  ChartData,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
 
-function App() {
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip
+);
+
+const labels = [
+  'Comida fria',
+  'Demora na entrega',
+  'Cabelo na comida',
+  'Comida muito salgada',
+  'Pedido cancelado',
+  'Travamento no App',
+  'Comida estragada',
+];
+
+export const data: ChartData = {
+  labels,
+  datasets: [
+    {
+      type: 'line',
+      label: 'Percentual acumulado',
+      borderColor: 'rgb(255, 99, 132)',
+      borderWidth: 2,
+      fill: false,
+      data: [28.2, 53.8, 66.6, 78.1, 87.1, 94.8, 100],
+      yAxisID: 'x2',
+    },
+    {
+      type: 'bar',
+      label: 'Quantidade',
+      backgroundColor: 'rgb(75, 192, 192)',
+      data: [22, 20, 10, 9, 7, 6, 4],
+      borderColor: 'white',
+      borderWidth: 2,
+      yAxisID: 'x1',
+    },
+  ],
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      type='bar'
+      data={data}
+      title='Gráfico de pareto'
+      options={{
+        scales: {
+          x2: {
+            position: 'right',
+            ticks: {
+              // @ts-ignore
+              callback: function (v: number) {
+                return `${this.getLabelForValue(v)}%`;
+              },
+            },
+          },
+        },
+      }}
+    />
   );
 }
-
-export default App;
